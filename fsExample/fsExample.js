86fda6d1ea20551b0cefd71dc1e75535b54ef748
srhtkemal <97290537+srhtkemal@users.noreply.github.com>
2022-07-27 13:31:28 +0300

    Add files via upload

---
+++ fsExample/fsExample.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+//read data
+fs.readFile("employees.json", "utf8",(err,data) => {
+       if (data) { console.log(data)     
+        console.log("Document readed")} 
+       else {
+         console.log(err);
+        }})
+
+// //write data
+// fs.appendFile("employees.json",'\n{"name":"employee2", "salary":3000}',"utf8" ,(err,data) => {
+//   if (data) { console.log(data)     
+//    console.log("Document updated")} 
+//   else {
+//     console.log(err);
+//    }})
+
+// //delete document
+// fs.unlink("employees.json",(err,data) => {
+//   if (data) { console.log(data)     
+//    console.log("Document deleted")} 
+//   else {
+//     console.log(err);
+//    }})
+
+
+
